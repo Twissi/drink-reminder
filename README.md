407e81ceaef5b3964394789f4da0257015ebd838
Kristin Fritsch <ich@kristin-fritsch.de>
2021-01-09 19:28:36 +0100

    Add documentation

---
+++ README.md
@@ -0,0 +1,117 @@
+# Water drink reminder
+
+## About
+
+When sitting on the desk all day i tend to forget to drink water.
+
+This projects visually reminds me to drink more water and also shows the progress I make throught the day to reach my drinking goal.
+
+Stay hydrated! 💦 💦
+
+![Desk](./images/desk.jpg)
+
+## Parts list
+
+- Arduino Uno
+- Neopixel Strip
+- HX711 Weighing Sensor Module with load cell
+- 300Ω - 500Ω Resistor
+
+- 5V Power Source
+- USB Cable
+- BreadBoard
+- Jump Wires
+
+## Parts images
+
+| Name                                        | Image                                          |
+| ------------------------------------------- | ---------------------------------------------- |
+| HX711 Weighing Sensor Module with load cell | ![Weight cell](./images/hx711-weight-cell.jpg) |
+
+## Libraries
+
+HX711_ADC: https://github.com/olkal/HX711_ADC
+Adafruit_NeoPixel: https://github.com/adafruit/Adafruit_NeoPixel
+
+## Wiring
+
+![Setup](./circuit_diagrams/setup.jpg)
+
+## Weight cell calibration
+
+Please follow the calibration from the example file `Calibration.ino` of [HX711 lib](https://github.com/olkal/HX711_ADC/blob/master/examples/Calibration/Calibration.ino).
+
+## Configuration
+
+In the `drink-reminder.ino` you need to configure the following values:
+
+Make sure the pins match with your setup:
+
+- HX711_dout = 4; (HX711 dout pin)
+- HX711_sck = 5; (HX711 sck pin)
+- LEDSTRIP_pin = 11; (led strip data pin)
+
+Update the following variables:
+
+- LEDS_COUNT = 15; (count of the leds you led strip has)
+- REMINDER_INTERVAL = 600000; (10 min reminder interval)
+- WATERMAX_ML = 2000; (2000 ml drink target)
+- WEIGHT_CALIBRATION_VALUE = 385.46; (load cell calibration value)
+
+## Usage
+
+1. Connect the Arduino via USB to your computer
+2. Connect the Ledstrip to the external power source
+3. Before starting the program remove the glas from the weight
+4. Upload the sketch file to your Arduino
+5. The led will blink in red during the startup phase
+6. After the led shows red (without blinking), put on the glas filled with water
+7. When removing or adding the glass the led will blink purple (measuring)
+8. Use the glass normally. Refill when needed
+9. When putting the glas back on the weight the leds will show you your status (you start with one green led and you reached your goal when all leds show green)
+10. Each day you need to restart the program in order to start with total water amount of 0
+
+## Color coding
+
+**Blinking red led**: Starting up
+
+**Red led**: Glas is missing
+
+**Blinking purple led**: Measurement in progress
+
+**Blinking blue leds**: Drink reminder!
+
+**Green led**: Total of water you drank today (1 pixel to max pixel count)
+
+## FAQ
+
+> How do i restart the program?
+
+You can use the reset button on the Arduino.
+
+> Do i need to subtract the weight of my glas?
+
+No, the weight of the glas does not matter. Although you should not use glasses with different weight throughout a day.
+
+> Can i power the ledstrip and the weight sensor module throught the same Arduino?
+
+Yes, you should be able to. But i run into issue with the weight changing when turning LEDs on vs. off. Thats why i switched to power the ledstrip with an external power source.
+
+> Do i need the resistor?
+
+I don't use it but it's recommended by Adafruit: https://learn.adafruit.com/adafruit-neopixel-uberguide/best-practices
+
+> Are you missing a capacitor?
+
+You are probably right. A capacitor is recommended by Adafruit: https://learn.adafruit.com/adafruit-neopixel-uberguide/best-practices
+
+> The drinking goal status doesn't work when I power the Arduino from an external power source.
+
+Yes, I am still trying to figure out why. :D
+
+## ToDos
+
+- fix goal status when powering Arduino with external power source
+- 3d print a case
+- transition setup and code to a smaller board
+- move all components + leds into the case
---
+++ arduino_sketches/drink-reminder/drink-reminder.ino
@@ -0,0 +1,246 @@
+/*
+   -------------------------------------------------------------------------------------
+   Drink reminder
+
+   Uses the following libraries:
+   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
+   Arduino library for Adafruit Neopixel
+   -------------------------------------------------------------------------------------
+*/
+
+/*
+   Before uploading the program calibrate the weight and update the WEIGHT_CALIBRATION_VALUE
+   
+   Before starting the program remove the glas from the weight
+   After the led shows red, put on the glas filled with water
+   Each day you need to restart the program in order to start with 0 total water amount
+
+   Color coding:
+   Blinking red led: Starting up
+   Red led: glas is missing
+   Blinking purple led: measurement in progress
+   Blinking blue leds: Drink reminder!
+   Green led: Total of water you drank today (1-15 pixels)
+*/
+
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#include <HX711_ADC.h>
+#include <EEPROM.h>
+#include <Adafruit_NeoPixel.h>
+
+// pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+const int LEDSTRIP_pin = 11; // led strip data pin
+
+// configuration
+const int LEDS_COUNT = 15;
+const long REMINDER_INTERVAL = 600000; //10 min reminder interval
+const int WATERMAX_ML = 2000; //2000 ml drink target
+const float WEIGHT_CALIBRATION_VALUE = 385.46; //calibration value (see example file "Calibration.ino" of HX711 lib)
+
+// logs:
+const bool LOG_WEIGHT = 0;
+const bool LOG_UNSTABLE_MEASUREMENT = 0;
+const bool LOG_TOTAL_AMOUNT_PIXEL = 0;
+const bool LOG_LAST_WEIGHT = 0;
+
+// HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+// NeoPixel constructor:
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS_COUNT, LEDSTRIP_pin, NEO_GRB + NEO_KHZ800);
+
+
+// Global variables
+long prevTime;
+float totalWaterAmount = 0;
+bool isLoaded = 0;
+bool prevIsLoaded = 0;
+float lastWeightLoaded;
+float lastWeightMeasurement = 0;
+long lastReminderMillis = 0;
+uint32_t lastLedColor = strip.Color(0, 0, 0, 255);
+uint32_t savedLedColor = strip.Color(0, 0, 0, 255);
+
+void setup() {
+  Serial.begin(57600); delay(10);
+  Serial.println();
+  Serial.println("Starting...");
+
+  // strip setup
+  strip.begin();
+  displayBlinkingColor(strip.Color(255, 0, 0), 50);
+
+  LoadCell.begin();
+  float calibrationValue = WEIGHT_CALIBRATION_VALUE;
+  long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+    displayColor(strip.Color(255, 0, 0), 0);
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; // increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset
+  if (newDataReady) {
+    if (millis() > prevTime + serialPrintInterval) {
+      float i = LoadCell.getData();
+      // action to be done after new weight was measured
+      onNewWeight(i);
+      newDataReady = 0;
+      prevTime = millis();
+    }
+  }
+
+  // drink reminder
+  if (millis() >= lastReminderMillis + REMINDER_INTERVAL) {
+    Serial.println("Don't forget to drink water!");
+    lastReminderMillis = millis();
+    saveLedColor(lastLedColor);
+    displayBlinkingColor(strip.Color(0, 0, 127), 50);
+    resetLedColor();
+  }
+}
+
+// On weight cell load - new weight found
+void onNewWeight(float weight) {
+  if (LOG_WEIGHT) {
+    Serial.print("Current weight: ");
+    Serial.println(weight);
+  }
+
+  if (!isStableValue(weight)) {
+    return;
+  }
+
+  if (weight < 1.00) {
+    isLoaded = 0;
+  } else {
+    isLoaded = 1;
+  }
+
+
+  // detect state change (glas was put on)
+  if (isLoaded != prevIsLoaded) {
+    prevIsLoaded = isLoaded;
+
+    if (isLoaded) {
+      if (LOG_LAST_WEIGHT) {
+        Serial.print("Last onload weight: ");
+        Serial.println(lastWeightLoaded);
+      }
+      
+      if (!lastWeightLoaded) {
+        lastWeightLoaded = weight;
+        displayColor(strip.Color(0, 255, 0), 50); // Green led = glas found
+        displayColor(strip.Color(0, 0, 0, 255), 0); // White
+        return;
+      }
+      // check if water decreased
+      if (lastWeightLoaded > weight + 1.00) {
+        totalWaterAmount = totalWaterAmount + lastWeightLoaded - weight;
+        // reset reminder
+        lastReminderMillis = millis();
+        Serial.print("Good job, water decreased! Your drank in total:");
+        Serial.println(totalWaterAmount);
+      }
+      lastWeightLoaded = weight;
+
+      displayTotalWaterAmount(strip.Color(0, 255, 0), 50); // Green led = glas found, show total amount
+      displayColor(strip.Color(0, 0, 0, 255), 0);
+    } else {
+      displayColor(strip.Color(255, 0, 0), 0); // Red led = glas missing
+    }
+  }
+}
+
+// return if weight is stable or not
+bool isStableValue(float weight) {
+  if (abs(lastWeightMeasurement) + 0.20 > abs(weight) && abs(lastWeightMeasurement) - 0.20 < abs(weight)) {
+    lastWeightMeasurement = weight;
+    return true;
+  }
+
+  if (LOG_UNSTABLE_MEASUREMENT) {
+    Serial.println("Unstable measurement, please wait!");
+  }
+  displayColor(strip.Color(255, 0, 213), 10);
+  displayColor(strip.Color(0, 0, 0, 255), 0); // White
+  lastWeightMeasurement = weight;
+  return false;
+}
+
+// save led color
+void saveLedColor(uint32_t c) {
+  savedLedColor = c;
+}
+
+// set led strip to saved led color
+void resetLedColor() {
+  displayColor(savedLedColor, 0);
+}
+
+// LEDStrip animation: Fill the dots one after the other with a color
+void displayColor(uint32_t c, uint8_t wait) {
+  lastLedColor = c;
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// LEDStrip animation: Only fill as many dots mapping the totalWaterAmount in percent
+void displayTotalWaterAmount(uint32_t c, uint8_t wait) {
+  int amountPerPixel = floor(WATERMAX_ML / strip.numPixels());
+  int numberOfPixels = ceil(totalWaterAmount / amountPerPixel);
+
+  if (numberOfPixels > strip.numPixels()) {
+    numberOfPixels = strip.numPixels();
+  }
+  if (LOG_TOTAL_AMOUNT_PIXEL) {
+    Serial.print("Show total amount pixel(s): ");
+    Serial.println(numberOfPixels);
+  }
+  lastLedColor = c;
+  for (uint16_t i = 0; i < numberOfPixels; i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// LEDStrip animation: Theatre-style crawling lights.
+void displayBlinkingColor(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
